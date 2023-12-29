a7160f38525e3dbcd3a38f818d378a90be948cb0
yuta1708 <yyuta750@gmail.com>
2023-12-29 15:02:57 +0900

    feat: #20 アイコン画像を円形に表示できるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :icon do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :thumb, resize_to_fill: [50, 50]
   end
 
   validates :username, presence: true
