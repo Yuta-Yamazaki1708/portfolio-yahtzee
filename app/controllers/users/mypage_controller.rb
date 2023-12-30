b0b32a7c7f632bc3f6f7deeb108ebcca82d8448b
yuta1708 <yyuta750@gmail.com>
2023-12-30 12:10:45 +0900

    refactor: 不要なインスタンス変数を削除する@userを削除する

--- app/controllers/users/mypage_controller.rb
+++ app/controllers/users/mypage_controller.rb
@@ -1,17 +1,15 @@
 class Users::MypageController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
   end
 
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
-    if @user.update(profile_params)
-      redirect_to mypage_path(@user)
+    if current_user.update(profile_params)
+      redirect_to mypage_path(current_user)
     else
       render action: :edit
     end
