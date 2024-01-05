00af77f4fb35f78e7c0b40aeead6d95ee84b9668
yuta1708 <yyuta750@gmail.com>
2024-01-05 15:55:03 +0900

    test: #39 capybaraでseleniumのremote_driverの設定をする

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "Users", type: :system, js: true do
   describe "ページ遷移テスト" do
     it "ログイン画面から新規登録画面へ遷移できること" do
       visit new_user_session_path
