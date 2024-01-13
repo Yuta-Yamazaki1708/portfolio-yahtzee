99b7ac6fa5203a7b1bf6a49f869c1a556d5bc26a
yuta1708 <yyuta750@gmail.com>
2024-01-13 15:58:31 +0900

    feat: #50 サイコロを最大3回までしか振れないようにする

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  before_action :check_execution_count, only: [:roll_dices]
+
   INIT_DICE_NUM = 5
 
   def new_game
@@ -9,7 +11,7 @@ class GamesController < ApplicationController
 
   def game
     @game = Game.find(params[:id])
-    @table_dices = [1, 1, 1, 1, 1]
+    @table_dices = ["dice", "dice", "dice", "dice", "dice"]
     @keep_dices = []
   end
 
@@ -18,16 +20,14 @@ class GamesController < ApplicationController
     @table_dices = params[:table_dices]
     dice_num = @table_dices.size
     @table_dices = @game.roll_dices(dice_num)
-    @keep_dices = params[:keep_dices]
-    @keep_dices = [] if @keep_dices.nil?
-    render "roll_dices", formats: :turbo_stream
+    @keep_dices = params[:keep_dices] || []
+    render "select_dice", formats: :turbo_stream
   end
 
   def move_to_keep
     @game = Game.find(params[:id])
     @table_dices = params[:from_dices]
-    @keep_dices = params[:to_dices]
-    @keep_dices = [] if @keep_dices.nil?
+    @keep_dices = params[:to_dices] || []
     index = params[:index].to_i
     @game.move(@table_dices, @keep_dices, index)
     render "select_dice", formats: :turbo_stream
@@ -36,10 +36,24 @@ class GamesController < ApplicationController
   def move_to_table
     @game = Game.find(params[:id])
     @keep_dices = params[:from_dices]
-    @table_dices = params[:to_dices]
-    @table_dices = [] if @table_dices.nil?
+    @table_dices = params[:to_dices] || []
     index = params[:index].to_i
     @game.move(@keep_dices, @table_dices, index)
     render "select_dice", formats: :turbo_stream
+  end
+
+  private
+
+  def check_execution_count
+    @game = Game.find(params[:id])
+    @table_dices = params[:table_dices]
+    @keep_dices = params[:keep_dices] || []
+    if session[:execution_count].to_i < 3
+      session[:execution_count] ||= 0
+      session[:execution_count] += 1
+    else
+      flash.now.alert = "サイコロを振れるのは3回までです。"
+      render "select_dice", formats: :turbo_stream
+    end
   end
 end
