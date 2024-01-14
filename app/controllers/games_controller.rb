fab777216b28a830987830722d8f2fa3d318bc92
yuta1708 <yyuta750@gmail.com>
2024-01-14 16:09:29 +0900

    feat: #51 サイコロの目によって得点を計算できるようにする

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
-  before_action :check_execution_count, only: [:roll_dices]
+  before_action :check_roll_count, only: [:roll_dices]
 
-  INIT_DICE_NUM = 5
+  DICE_NUM = 5
+  TIMES_OF_ROLL_DICES = 3
 
   def new_game
     @game = Game.new
@@ -10,50 +11,68 @@ class GamesController < ApplicationController
   end
 
   def game
+    session[:execution_count] = 0
     @game = Game.find(params[:id])
-    @table_dices = ["dice", "dice", "dice", "dice", "dice"]
+    @categories_and_results = @game.display_results
+    @table_dices = Array.new(DICE_NUM) { "Dice" }
     @keep_dices = []
+    @calculated_scores = {}
   end
 
   def roll_dices
     @game = Game.find(params[:id])
-    @table_dices = params[:table_dices]
+    @categories_and_results = @game.display_results
+    @table_dices = param_to_integer(params[:table_dices])
     dice_num = @table_dices.size
     @table_dices = @game.roll_dices(dice_num)
-    @keep_dices = params[:keep_dices] || []
-    render "select_dice", formats: :turbo_stream
+    @keep_dices = param_to_integer(params[:keep_dices]) || []
+    @calculated_scores = @game.calculate_scores(@table_dices + @keep_dices)
+    render "update_dices", formats: :turbo_stream
   end
 
   def move_to_keep
     @game = Game.find(params[:id])
-    @table_dices = params[:from_dices]
-    @keep_dices = params[:to_dices] || []
+    @categories_and_results = @game.display_results
+    @table_dices = param_to_integer(params[:from_dices])
+    @keep_dices = param_to_integer(params[:to_dices]) || []
     index = params[:index].to_i
     @game.move(@table_dices, @keep_dices, index)
-    render "select_dice", formats: :turbo_stream
+    @calculated_scores = @game.calculate_scores(@table_dices + @keep_dices)
+    render "update_dices", formats: :turbo_stream
   end
 
   def move_to_table
     @game = Game.find(params[:id])
-    @keep_dices = params[:from_dices]
-    @table_dices = params[:to_dices] || []
+    @categories_and_results = @game.display_results
+    @keep_dices = param_to_integer(params[:from_dices])
+    @table_dices = param_to_integer(params[:to_dices]) || []
     index = params[:index].to_i
     @game.move(@keep_dices, @table_dices, index)
-    render "select_dice", formats: :turbo_stream
+    @calculated_scores = @game.calculate_scores(@table_dices + @keep_dices)
+    render "update_dices", formats: :turbo_stream
   end
 
   private
 
-  def check_execution_count
+  def check_roll_count
     @game = Game.find(params[:id])
-    @table_dices = params[:table_dices]
-    @keep_dices = params[:keep_dices] || []
-    if session[:execution_count].to_i < 3
+    @categories_and_results = @game.display_results
+    @table_dices = param_to_integer(params[:table_dices])
+    @keep_dices = param_to_integer(params[:keep_dices]) || []
+    @calculated_scores = @game.calculate_scores(@table_dices + @keep_dices)
+    if session[:execution_count].to_i < TIMES_OF_ROLL_DICES
       session[:execution_count] ||= 0
       session[:execution_count] += 1
     else
       flash.now.alert = "サイコロを振れるのは3回までです。"
-      render "select_dice", formats: :turbo_stream
+      render "update_dices", formats: :turbo_stream
     end
+  end
+
+  def param_to_integer(array_of_param)
+    if array_of_param.nil?
+      return
+    end
+    array_of_param.map { |string| string.to_i }
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,32 @@
 class Game < ApplicationRecord
+  FACE_NUM = 6
+  CATEGORIES = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "bonus",
+    "choice",
+    "four_dices",
+    "full_house",
+    "small_straight",
+    "big_straight",
+    "yacht",
+    "sum",
+  ].freeze
+
+  # インスタンスメソッド
+
+  def display_results
+    CATEGORIES.index_with { |category| public_send(category) }
+  end
+
+  # クラスメソッドにした方がいいかも？
+
   def roll_dices(dice_num)
-    dices = Array.new(dice_num) { rand(1..6) }
+    dices = Array.new(dice_num) { rand(1..FACE_NUM) }
     dices
   end
 
@@ -8,5 +34,60 @@ class Game < ApplicationRecord
     tmp = from_array[index]
     from_array.delete_at(index)
     to_array.push(tmp)
+  end
+
+  def calculate_scores(array)
+    if array.include?(0)
+      return CATEGORIES.index_with { |category| nil }
+    end
+    caluculated_scores = {}
+
+    caluculated_scores["one"] ||= array.count { |num| num == 1 }
+
+    caluculated_scores["two"] = array.count { |num| num == 2 } * 2
+
+    caluculated_scores["three"] = array.count { |num| num == 3 } * 3
+
+    caluculated_scores["four"] = array.count { |num| num == 4 } * 4
+
+    caluculated_scores["five"] = array.count { |num| num == 5 } * 5
+
+    caluculated_scores["six"] = array.count { |num| num == 6 } * 6
+
+    caluculated_scores["choice"] = array.sum
+
+    counts = array.uniq.map { |num| array.count(num) }
+    caluculated_scores["four_dices"] = counts.include?(4) || counts.include?(5) ? array.sum : 0
+
+    counts = array.uniq.map { |num| array.count(num) }
+    caluculated_scores["full_house"] = counts.include?(3) && counts.include?(2) ? array.sum : 0
+
+    sorted_dice = array.uniq.sort
+    caluculated_scores["small_straight"] = check_s_straight(sorted_dice) == true ? 15 : 0
+
+    sorted_dice = array.uniq.sort
+    caluculated_scores["big_straight"] = check_b_straight(sorted_dice) == true ? 30 : 0
+
+    caluculated_scores["yacht"] = array.uniq.size == 1 ? 50 : 0
+
+    caluculated_scores
+  end
+
+  private
+
+  def check_s_straight(sorted_array)
+    sorted_array.each_cons(4) do |subarray|
+      if subarray.each_cons(2).all? { |a, b| b == a + 1 }
+        return true
+      end
+    end
+  end
+
+  def check_b_straight(sorted_array)
+    sorted_array.each_cons(5) do |subarray|
+      if subarray.each_cons(2).all? { |a, b| b == a + 1 }
+        return true
+      end
+    end
   end
 end
