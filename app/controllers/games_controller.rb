3bf4a53d21311d3891293dc672a568b2bc0ae4ba
yuta1708 <yyuta750@gmail.com>
2024-01-22 17:19:09 +0900

    refactor:　#67 サイコロの出目による得点の計算結果をsessionで扱うようにする

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  before_action :init_sessions, only: [:game]
   before_action :check_reload, only: [:game]
   before_action :check_roll_count, only: [:roll_dices]
   before_action :check_turn_count, only: [:select_category]
@@ -18,53 +19,60 @@ class GamesController < ApplicationController
   end
 
   def game
-    session[:roll_count] = nil
-    session[:table_dices] = Array.new(DICE_NUM) { 0 }
-    session[:keep_dices] = []
     @game = Game.find(params[:id])
     @categories_and_results = @game.display_results
     @table_dices = session[:table_dices]
     @keep_dices = session[:keep_dices]
-    @calculated_scores = {}
+    @calculated_scores = session[:calculated_scores]
   end
 
   def roll_dices
     dice_num = session[:table_dices].size
     session[:table_dices] = Game.roll_dices(dice_num)
-    session[:keep_dices] = session[:keep_dices] || []
+    session[:calculated_scores] = Game.calculate_scores(session[:table_dices] + session[:keep_dices])
+
     @game = Game.find(params[:id])
     @categories_and_results = @game.display_results
-    @calculated_scores = Game.calculate_scores(session[:table_dices] + session[:keep_dices])
     @table_dices = session[:table_dices]
     @keep_dices = session[:keep_dices]
+    @calculated_scores = session[:calculated_scores]
+
     render "roll_dices", formats: :turbo_stream
   end
 
   def move_to_keep
-    @game = Game.find(params[:id])
-    @table_dices = param_to_integer(params[:from_dices])
-    @keep_dices = param_to_integer(params[:to_dices]) || []
     index = params[:index].to_i
-    Game.move(@table_dices, @keep_dices, index)
+    Game.move(session[:table_dices], session[:keep_dices], index)
+
+    @game = Game.find(params[:id])
+    @table_dices = session[:table_dices]
+    @keep_dices = session[:keep_dices]
   end
 
   def move_to_table
-    @game = Game.find(params[:id])
-    @keep_dices = param_to_integer(params[:from_dices])
-    @table_dices = param_to_integer(params[:to_dices]) || []
     index = params[:index].to_i
-    Game.move(@keep_dices, @table_dices, index)
+    Game.move(session[:keep_dices], session[:table_dices], index)
+
+    @game = Game.find(params[:id])
+    @keep_dices = session[:keep_dices]
+    @table_dices = session[:table_dices]
   end
 
   def select_category
     @game = Game.find(params[:id])
-    @game.update(params[:category] => params[:calculated_score].to_i)
+    @game.update(params[:category] => session[:calculated_scores][params[:category]])
     @game.update("bonus" => bonus(@game))
     @game.update("sum" => sum(@game))
-    @table_dices = Array.new(DICE_NUM) { 0 }
-    @keep_dices = []
+
+    session[:table_dices] = Array.new(DICE_NUM) { 0 }
+    session[:keep_dices] = []
+    session[:calculated_scores] = {}
+
+    @table_dices = session[:table_dices]
+    @keep_dices = session[:keep_dices]
     @categories_and_results = @game.display_results
-    @calculated_scores = {}
+    @calculated_scores = session[:calculated_scores]
+
     if session[:turn_count].to_i < TURN_NUM
       render "select_category", formats: :turbo_stream
     else
@@ -73,7 +81,7 @@ class GamesController < ApplicationController
     end
   end
 
-  # javascriptで使用する変数をjsonAPIで出力する
+  # javascriptで使用する変数をjsonAPIで出力する.
   def get_roll_count
     render json: {
       roll_count: session[:roll_count],
@@ -83,21 +91,27 @@ class GamesController < ApplicationController
 
   private
 
+  def init_sessions
+    session[:roll_count] = nil
+    session[:table_dices] = Array.new(DICE_NUM) { 0 }
+    session[:keep_dices] = []
+    session[:calculated_scores] = {}
+  end
+
   # サイコロを振った回数を記録する.
   def check_roll_count
-    session[:table_dices] = session[:table_dices]
-    session[:keep_dices] = session[:keep_dices] || []
-    @game = Game.find(params[:id])
-    @categories_and_results = @game.display_results
-    @calculated_scores = Game.calculate_scores(session[:table_dices] + session[:keep_dices])
-    @table_dices = session[:table_dices]
-    @keep_dices = session[:keep_dices]
     if session[:roll_count].to_i < TIMES_OF_ROLL_DICES
       session[:roll_count] ||= 0
       session[:roll_count] += 1
     else
       session[:roll_count] += 1
       flash.now.alert = "サイコロを振れるのは3回までです。"
+      @game = Game.find(params[:id])
+      @categories_and_results = @game.display_results
+      @calculated_scores = Game.calculate_scores(session[:table_dices] + session[:keep_dices])
+      @table_dices = session[:table_dices]
+      @keep_dices = session[:keep_dices]
+
       render "roll_dices", formats: :turbo_stream
     end
   end
@@ -116,16 +130,9 @@ class GamesController < ApplicationController
       game = Game.find(params[:id])
       game.destroy
       flash.notice = "リロードされたためゲームを中止しました。"
+
       redirect_to root_path
     end
-  end
-
-  # 文字列で送られてきたパラムを数値に変換する、一番最初は値がnilなためreturn.
-  def param_to_integer(array_of_param)
-    if array_of_param.nil?
-      return
-    end
-    array_of_param.map(&:to_i)
   end
 
   def bonus(game)
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,26 +1,15 @@
 module GamesHelper
-  def move_btn_params(game, from_dices, to_dices, index)
+  def move_btn_params(game, index)
     {
       id: game.id,
-      from_dices: from_dices,
-      to_dices: to_dices,
       index: index,
     }
   end
 
-  def roll_dices_params(game, table_dices, keep_dices)
-    {
-      id: game.id,
-      table_dices: table_dices,
-      keep_dices: keep_dices,
-    }
-  end
-
-  def select_category_params(game, category, calculated_score)
+  def select_category_params(game, category)
     {
       id: game.id,
       category: category,
-      calculated_score: calculated_score,
     }
   end
 
